f2ecfb2455112b651485c2aae57426bb5044a9ed
Minuli Lavanya <minuli.bambarandage@axiatadigitallabs.com>
2025-04-28 20:42:38 +0530

    setup environment files

---
+++ Test-02/car-service-center/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiBaseUrl: 'https://api.production-domain.com/api',
+    language: 'en'
+  };
+  
---
+++ Test-02/car-service-center/src/environments/environment.staging.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiBaseUrl: 'https://api.staging-domain.com/api',
+    language: 'en'
+  };
+  
