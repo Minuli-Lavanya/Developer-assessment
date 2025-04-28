a1af416e14a2392b4d00c6f3459d146a2f8e9e5d
Minuli Lavanya <minuli.bambarandage@axiatadigitallabs.com>
2025-04-28 20:52:48 +0530

    add Dashboard

---
+++ Test-02/car-service-center/src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { CarService } from '../../services/car.service';
+import { JobService } from '../../services/job.service';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent implements OnInit {
+
+  totalCars = 0;
+  pending = 0;
+  completed = 0;
+
+  constructor(private carService: CarService, private jobService: JobService) {}
+
+  ngOnInit() {
+    this.carService.getCars().subscribe((cars: any) => {
+      this.totalCars = cars.length;
+    });
+
+    this.jobService.getJobs().subscribe((jobs: any) => {
+      this.pending = jobs.filter((j: any) => j.status === 'Pending').length;
+      this.completed = jobs.filter((j: any) => j.status === 'Completed').length;
+    });
+  }
+}
